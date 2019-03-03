c0acd7825254328b2375176842b46895e40776c3
Jakub Rodek <jakubrode@gmail.com>
2019-03-03 21:35:50 +0100

    Calculated final frequency of challenge day 1

---
+++ AdventOfCode/FileReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ChronalCalibration
+{
+    public class FileReader
+    {
+        public long SumNumbersFromFile(string FilePath)
+        {
+            long sum = 0;
+            
+            // Read the file
+            string[] textLines = System.IO.File.ReadAllLines(FilePath);
+
+            //convert each line to int32
+            foreach (var line in textLines)
+            {
+                int convertedInt = Int32.Parse(line);
+
+                sum += convertedInt;
+            }
+            
+            return sum;
+        }
+   
+    }
+}
---
+++ ChronalCalibration.Tests/Class1.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using ChronalCalibration;
+
+namespace ChronalCalibration.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void ReadFile()
+        {
+            var reader = new FileReader();
+            var sum = reader.SumNumbersFromFile(@"Input.txt");
+            Assert.AreEqual(518,sum);
+        }
+    }
+}
