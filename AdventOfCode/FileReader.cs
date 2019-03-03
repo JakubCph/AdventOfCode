690ec0ba9ba528cb414bf500bbd4a728ccb6ba19
Jakub Rodek <jakubrode@gmail.com>
2019-03-03 23:12:18 +0100

    Calculated repeated frequency

--- AdventOfCode/FileReader.cs
+++ AdventOfCode/FileReader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ChronalCalibration
 {
@@ -21,6 +22,41 @@ namespace ChronalCalibration
             
             return sum;
         }
-   
+
+        public long CalculateFirstRepeatedFrequency(string FilePath)
+        {
+            long sum = 0;
+            var savedValues = new List<long>();
+            savedValues.Add(0);
+
+            // Read the file
+            string[] sequence = System.IO.File.ReadAllLines(FilePath);
+
+            //convert each line to int32
+            while (true)
+            {
+
+                foreach (var line in sequence)
+                {
+                    int freqChange = Int32.Parse(line);
+
+                    sum += freqChange;
+
+                    // do we have a duplicate
+                    if (savedValues.Contains(sum))
+                    {
+                        return sum;
+                    }
+
+                    savedValues.Add(sum);
+                }     
+                
+            }
+
+       
+        }
+
+    
+
     }
 }
--- ChronalCalibration.Tests/Class1.cs
+++ ChronalCalibration.Tests/Class1.cs
@@ -13,5 +13,15 @@ namespace ChronalCalibration.Tests
             var sum = reader.SumNumbersFromFile(@"Input.txt");
             Assert.AreEqual(518,sum);
         }
+
+        [Test]
+        public void CalculateFirstRepeatedFrequencyTest()
+        {
+            var reader = new FileReader();
+            var repeatedFreq = reader.CalculateFirstRepeatedFrequency(@"Input.txt");
+            Assert.AreEqual(72889L, repeatedFreq);
+        }
+
+       
     }
 }
